95fc623b5884fd69f70732b67fcf1119dc16802e
Simone <simoneniro17@gmail.com>
2023-08-23 19:28:17 +0200

    Aggiunta commenti pattern Observer

--- src/main/java/com/example/CampusCarpool/engineering/observer/Subject.java
+++ src/main/java/com/example/CampusCarpool/engineering/observer/Subject.java
@@ -1,21 +1,22 @@
 package com.example.CampusCarpool.engineering.observer;
 
 import com.example.CampusCarpool.bean.RideRequestBean;
-import com.example.CampusCarpool.model.Ride;
 import javafx.scene.layout.Pane;
 
 import java.util.List;
 import java.util.Vector;
 
-// Classe astratta per impedirne l'istanziazione
+// Classe astratta per impedirne l'istanziazione diretta
 public abstract class Subject {
 
     private final List<Observer> observersList;
 
+    // Costruttore protetto senza argomenti
     protected Subject() {
         this((Observer) null);
     }
 
+    // Costruttore protetto con observer iniziale
     protected Subject(Observer observer) {
         this(new Vector<>());
 
@@ -24,18 +25,22 @@ public abstract class Subject {
         }
     }
 
+    // Costruttore protetto con lista di observer
     protected Subject(List<Observer> observersList) {
         this.observersList = observersList;
     }
 
+    // Registra un observer
     public void register(Observer observer) {
         observersList.add(observer);
     }
 
+    // Rimuove un observer
     public void unregister(Observer observer) {
         observersList.remove(observer);
     }
 
+    // Modifica tutti gli observer e aggiorna la pagina del guidatore
     public void notifyObserversDriver(RideRequestBean rideRequestBean, Pane pane) {
         for (Observer observer : observersList) {
             observer.update();
@@ -43,6 +48,7 @@ public abstract class Subject {
         }
     }
 
+    // Modifica tutti gli observer e aggiorna la pagina del passeggero
     public void notifyObserversPassenger(RideRequestBean rideRequestBean, Pane pane) {
         for (Observer observer : observersList) {
             observer.update();
